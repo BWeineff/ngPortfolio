847b6fc3305cd216df1305c5facbc8d6e76a51ff
Benjamin Weinberger <benjaminweinberger@gmail.com>
2019-03-15 16:02:41 -0700

    march 15 (3)

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -8,14 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class PortfolioPageComponent implements OnInit {
   matCards = [
     {
-      title: 'Shiba Inu',
+      title: 'ML5 Image Video Recognition',
       subtitle: 'Dog Breed',
-      imgSrc: "https://material.angular.io/assets/img/examples/shiba2.jpg",
-      description: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
-      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
-      bred for hunting.`,
-      buttonTextOne: 'LIKE',
-      buttonTextTwo: 'SHARE'
+      imgSrc: "/assets/images/thatsAFrog.jpeg",
+      description: `This is a quick demo website to teach myself how to use ML5 tensorflow.js and video playback recognition. The site is a blank shell that is mobile and desktop friendly, using the user's camera to pull a live feed and run it through an image library to give automatic guess for image recognition (will not work with people or with lots of background motion). This was demoed by the coding train tutorials and I built this version as best I could to emulate it within my first 2 months of coding.`,
+      buttonTextOne: 'CLICK',
+      buttonTextTwo: 'SHARE',
+      website: 'https://bweineff.github.io/ml5VideoCapture/',
     },
     {
       title: 'Shiba Inu',
